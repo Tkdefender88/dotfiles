836bcc6477d6c610a4cee015a5b186a392a05c04
tkdefender <tkdefender@proton.me>
2025-08-27 12:03:56 -0600

    update keybind for tmux sessionizer

--- env/config/nvim/lua/tkdefender/remap.lua
+++ env/config/nvim/lua/tkdefender/remap.lua
@@ -35,9 +35,4 @@ vim.keymap.set("n", "<leader>dm", function() vim.opt.background="dark" end, {des
 vim.keymap.set("n", "<leader>lm", function() vim.opt.background="light" end, {desc="toggle dark mode"})
 
 -- tmux sessionizer
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
-vim.keymap.set("n", "<M-t>", "<cmd>silent !tmux neww tmux-sessionizer -s 1<CR>")
-vim.keymap.set("n", "<M-n>", "<cmd>silent !tmux neww tmux-sessionizer -s 2<CR>")
-vim.keymap.set("n", "<M-s>", "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>")
-
+vim.keymap.set("n", "<M-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")

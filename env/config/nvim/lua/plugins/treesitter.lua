8c40cac4cfd34fda026c46bb6d97f741f68a2a85
Justin Bak (╯°□°)╯︵ ┻━┻ <me@justinbak.com>
2026-07-17 10:09:16 -0600

    fix neovim plugins

--- env/config/nvim/lua/plugins/treesitter.lua
+++ env/config/nvim/lua/plugins/treesitter.lua
@@ -4,24 +4,40 @@ return {
     branch = 'main',
     build = ':TSUpdate',
     config = function()
+      local ensure_installed = {
+        "javascript",
+        "typescript",
+        "go",
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+        "markdown_inline",
+        "gleam",
+        "zig",
+        "rust",
+        "python",
+      }
+
       require('nvim-treesitter').setup({
         prefer_git = true,
-        ensure_installed = {
-          "javascript",
-          "typescript",
-          "go",
-          "c",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "markdown",
-          "markdown_inline",
-          "gleam",
-          "zig",
-          "rust",
-          "python",
-        },
+      })
+
+      local installed = require('nvim-treesitter.config').get_installed()
+      local missing = vim.tbl_filter(function(lang)
+        return not vim.tbl_contains(installed, lang)
+      end, ensure_installed)
+      if #missing > 0 then
+        require('nvim-treesitter').install(missing)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = '*',
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
       })
     end
 }
